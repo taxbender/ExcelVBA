c9955acc9a29c6e297f3d9c8775dff9497e61607
taxbender <akeene@gmail.com>
2015-11-30 14:52:24 -0600

    Update TestURL.vb

--- TestURL.vb
+++ TestURL.vb
@@ -3,22 +3,23 @@ Private Function TestURL(strUrl As String) As Boolean
     'Requires Microsoft WinHTTP Services, Version 5.1
     
     'Function returns TRUE is URL is found; FALSE if not found
+    'Default value of TestURL is FALSE
     
     Dim oURL As New WinHttpRequest
 
     On Error GoTo TestURL_Err
-     
+    
     With oURL
         .Open "GET", strUrl, False
         .send
         If .Status = 200 Then          '200 indicates resource was retrieved
             TestURL = True
         End If
-      TestURL = (.Status = 200)
     End With
      
 TestURL_Err:
+    'Invalid resources cause an error; Function returns default value
+    
     Set oURL = Nothing                 'Clean up object
-    TestURL = False                    'Invalid resources cause an error
 
 End Function
