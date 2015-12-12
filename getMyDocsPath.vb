743cbc6f045cc9fe1c2dc39e9dff9b792ea3f16f
taxbender <akeene@gmail.com>
2015-12-12 09:14:20 -0600

    Update getMyDocsPath.vb

--- getMyDocsPath.vb
+++ getMyDocsPath.vb
@@ -1,24 +1,16 @@
 Function getMyDocsPath() As String
   If gEnableErrorHandling Then On Error GoTo errHandler
-  
-  
-  
+ 
   Dim oShell As Object
   Set oShell = CreateObject("WScript.Shell")
   
   getMyDocsPath = oShell.SpecialFolders("mydocuments")
 
-
-
 exitHere:
   Set oShell = Nothing
-  
   Exit Function
 
 errHandler:
-  MsgBox "Error " & Err.Number & ": " & Err.Description & " in ", _
-          vbOKOnly, "Error"
-
-Resume exitHere
+  Resume exitHere
 
 End Function
