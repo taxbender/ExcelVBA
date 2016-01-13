95aa8ec824e3bf1a560dad0755d2ba3abcd675a2
taxbender <akeene@gmail.com>
2016-01-13 09:59:31 -0600

    Update getMyDocsPath.vb

--- getMyDocsPath.vb
+++ getMyDocsPath.vb
@@ -1,16 +1,32 @@
-Function getMyDocsPath() As String
-  If gEnableErrorHandling Then On Error GoTo errHandler
+Public Function getMyDocsPath() As String
+  
+  '******************************************************************************
+  ' Description: Retreives current user's My Documents path without trailing "/"
+  '
+  ' Author:       taxbender
+  ' Contributors:
+  ' Sources:
+  ' Last Updated: 12/30/2015
+  ' Dependencies: Runtime Reference:  Microsoft Scripting Runtime
+  '               Var - cEnableErrorHandling
+  '               Sub - errMessage
+  ' Known Issues: None
+  '******************************************************************************
+
+  If cEnableErrorHandling Then On Error GoTo errHandler
  
   Dim oShell As Object
+  
   Set oShell = CreateObject("WScript.Shell")
   
   getMyDocsPath = oShell.SpecialFolders("mydocuments")
-
-exitHere:
+  
+exitMe:
   Set oShell = Nothing
   Exit Function
 
 errHandler:
-  Resume exitHere
-
+  errMessage "getMyDocsPath", Err.Number, Err.Description
+  Resume exitMe
+  
 End Function
